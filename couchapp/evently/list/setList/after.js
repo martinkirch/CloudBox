c851dc527e37213caf80e29f48022ad094a515df
Martin Kirchgessner <martin.kirch@gmail.com>
2010-07-11 21:50:08 +0200

    Changed the 'url' generation to get rid of 'filename'. Now the MP3 URL is found with the _attachments array.

--- couchapp/evently/list/setList/after.js
+++ couchapp/evently/list/setList/after.js
@@ -7,9 +7,13 @@ function(e,data)
 	{
 		doc = data[i];
 		
-		if(!doc.url)
-			doc.url = prefix+encodeURIComponent(doc._id)+'/'+encodeURIComponent(doc.filename);
-		
+		if(!doc.url && doc._attachments)
+			for (var name in doc._attachments)
+				if(name.substring(name.length - 3) == 'mp3')
+				{
+					doc.url = prefix + encodeURIComponent(doc._id) + '/' + encodeURIComponent(name);
+				}
+		alert(doc.url);
 		$('#song_'+doc._id).data('doc', doc);
 	};
 }
